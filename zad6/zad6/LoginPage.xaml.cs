566cc96bd3e95b6554fbcf88356cc7c557b887c1
BulatDes <123715505+BulatDes@users.noreply.github.com>
2023-06-23 11:38:57 +0500

    Add files via upload

---
+++ zad6/zad6/LoginPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zad6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage :ContentPage
+    {
+        public LoginPage ()
+        {
+            InitializeComponent();
+        }
+        private async void InputButton_Clicked (object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(login.Text) || string.IsNullOrEmpty(pass.Text))
+            {
+                DisplayAlert("Внимание", "Заполните логин и пароль", "OK");
+                return;
+            }
+
+            await Navigation.PushModalAsync(new MainPage(login.Text, pass.Text));
+        }
+    }
+}
---
+++ zad6/zad6/Three.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zad6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Three :ContentPage
+    {
+        public Three (string login, string pass,double sl1, double sl2, double sl3, double sl4)
+        {
+            InitializeComponent();
+            LoginTxt.Text = $"Ваш логин: {login}";
+            PassTxt.Text = $"Ваш пароль: {pass}";
+            Slider1Txt.Text = $"Значение Ползунка 1: {sl1}";
+            Slider2Txt.Text = $"Значение Ползунка 2: {sl2}";
+            Slider3Txt.Text = $"Значение Ползунка 3: {sl3}";
+            Slider4Txt.Text = $"Значение Ползунка 4: {sl4}";
+        }
+    }
+}
