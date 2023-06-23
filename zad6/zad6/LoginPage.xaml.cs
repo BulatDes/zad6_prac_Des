2dd02a400b7f3e0c26f6e7d9a1c0b44e858e8d8b
BulatDes <123715505+BulatDes@users.noreply.github.com>
2023-06-23 14:28:15 +0500

    Add files via upload

---
+++ zad6/zad6/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zad6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage :CarouselPage
+    {
+        public LoginPage ()
+        {
+            InitializeComponent();
+        }
+        //Метод проверяющий вводные данные и переходящий на другой экран
+        private async void InputButton_Clicked (object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(login.Text) || string.IsNullOrEmpty(pass.Text))
+            {
+                DisplayAlert("Внимание", "Заполните логин и пароль", "OK");
+                return;
+            }
+
+            await Navigation.PushModalAsync(new MainPage(login.Text, pass.Text));
+        }
+    }
+}
